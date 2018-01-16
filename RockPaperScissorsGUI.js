8f6c0dbdb560b9b42d69b671955a2d71fa786035
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-01-16 16:14:48 -0500

    Worked on the reset function for the game

--- RockPaperScissorsGUI.js
+++ RockPaperScissorsGUI.js
@@ -5,7 +5,12 @@ const h4 = document.querySelector('h4');
 const h2 = document.querySelector('h2');
 
 function reset() {
-
+  const content = document.querySelector('p');
+  humanScore = 0; // scores reset to 0
+  computerScore = 0; // scores reset to 0
+  h4.textContent = 'Player: ${humanScore} Computer: ${computerScore}';
+  content.textContent = "";
+  h2.textContent = "";
 }
 
 function buttonReset() {
