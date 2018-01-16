af4e2096ccf47c9d42cfb7709cc28cc184521352
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-01-16 16:06:24 -0500

    GUI.js backbone

---
+++ RockPaperScissorsGUI.js
@@ -0,0 +1,29 @@
+let computerScore = 0;
+let humanScore = 0;
+
+const h4 = document.querySelector('h4');
+const h2 = document.querySelector('h2');
+
+function reset() {
+
+}
+
+function buttonReset() {
+
+}
+
+function computerChoice() {
+
+}
+
+function playRound() {
+
+}
+
+function game() {
+
+}
+
+function startGame() {
+
+}
